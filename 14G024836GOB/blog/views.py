4fdb4b3897aaa739dfca936850f50f0931a015a7
Evelyn Oviangbede <103006429+EvelynOvi@users.noreply.github.com>
2022-06-24 11:57:11 +0100

    crud update

--- 14G024836GOB/blog/views.py
+++ 14G024836GOB/blog/views.py
@@ -9,26 +9,28 @@ from django.views.generic.edit import (
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
+
 class PostListView(ListView):
     model = Post
 
+
 class PostCreateView(CreateView):
-    model=Post
+    model = Post
     fields = "__all__"
-    success_url = reverse_lazy("blog: all")
+    success_url = reverse_lazy("blog:all")
+
 
 class PostDetailView(DetailView):
     model = Post
 
+
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
-    success_url = reverse_lazy("blog: all")
+    success_url = reverse_lazy("blog:all")
+
 
 class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
-    success_url = reverse_lazy("blog: all")
-
-
- 
+    success_url = reverse_lazy("blog:all")
